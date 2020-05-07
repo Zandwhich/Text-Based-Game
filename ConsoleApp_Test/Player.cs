9c5cb128e47aa2123c57d4fe303a277d1c11f0c3
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2020-05-07 16:59:43 -0700

    Added some more variables to the player

--- ConsoleApp_Test/Player.cs
+++ ConsoleApp_Test/Player.cs
@@ -11,11 +11,15 @@ namespace ConsoleApp_Test
         // The name of the player
         private string name;
 
+        // The health of the player
+        private double health;
+
 
         // The constructor of the player
         public Player(string name)
         {
             this.name = name;
+            this.health = 100;
         }//end Player()
         
 
@@ -24,5 +28,18 @@ namespace ConsoleApp_Test
         {
             return this.name;
         }//end getName()
+
+        // Returns the health of the player
+        public double getHealth()
+        {
+            return this.health;
+        }//end getHealth()
+
+        // Returns if the player is dead or not
+        public bool isDead()
+        {
+            if (this.health <= 0) return true;
+            return false;
+        }//end isDead()
     }//end Player
 }//end ConsoleApp_Test
