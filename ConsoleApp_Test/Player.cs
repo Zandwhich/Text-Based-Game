6be5107debd3c95c54225ac2fc7856a92f9c1abf
Alex Zdanowicz <alex.zdanowicz@gmail.com>
2020-05-07 16:05:06 -0700

    Added the Player class into the project

---
+++ ConsoleApp_Test/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_Test
+{
+    // The class that defines the player
+    class Player
+    {
+
+        // The name of the player
+        private string name;
+
+
+        // The constructor of the player
+        public Player(string name)
+        {
+            this.name = name;
+        }//end Player()
+        
+
+        // Returns the name of the player
+        public string getName()
+        {
+            return this.name;
+        }//end getName()
+    }//end Player
+}//end ConsoleApp_Test
